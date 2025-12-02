2d9063f17f72b5e3221f85ae92f4ad0fb556f474
JMSBPP <jmsbpp@hotmail.es>
2025-12-01 21:02:46 -0500

    test: add MasterHook bytecode helper and update test setup
    - Add MasterHook0_8_26.sol helper with creation code bytecode
    - Update MasterHook.t.sol test file
    - Update foundry.toml configuration
    - Update DeployMasterHook script
    - Remove unused LibHooks.sol

--- contracts/hook-master-pkg/script/DeployMasterHook.s.sol
+++ contracts/hook-master-pkg/script/DeployMasterHook.s.sol
@@ -1,35 +1,35 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.19;
 
-import "forge-std/Script.sol";
-import "../src/MasterHook.sol";
-import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
-import {HookMiner} from "@uniswap/v4-periphery/src/utils/HookMiner.sol";
-import {AllHook} from "../src/AllHook.sol";
+// import "forge-std/Script.sol";
+// import "../src/MasterHook.sol";
+// import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+// import {HookMiner} from "@uniswap/v4-periphery/src/utils/HookMiner.sol";
+// import {AllHook} from "../src/AllHook.sol";
 
-contract DeployMasterHookScript is Script{
-    address constant CREATE2_DEPLOYER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
-    IPoolManager constant POOLMANAGER = IPoolManager(address(0xE03A1074c86CFeDd5C142C4F04F1a1536e203543));
+// contract DeployMasterHookScript is Script{
+//     address constant CREATE2_DEPLOYER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
+//     IPoolManager constant POOLMANAGER = IPoolManager(address(0xE03A1074c86CFeDd5C142C4F04F1a1536e203543));
 
-    function setUp() public {}
+//     function setUp() public {}
 
-    function run() public {
-        bytes memory constructorArgs = abi.encode(POOLMANAGER);
-        (address masterHookExpectedAddress, bytes32 salt) =
-            HookMiner.find(CREATE2_DEPLOYER, Hooks.ALL_HOOK_MASK, type(MasterHook).creationCode, abi.encode("0x00"));
-        vm.broadcast();
+//     function run() public {
+//         bytes memory constructorArgs = abi.encode(POOLMANAGER);
+//         (address masterHookExpectedAddress, bytes32 salt) =
+//             HookMiner.find(CREATE2_DEPLOYER, Hooks.ALL_HOOK_MASK, type(MasterHook).creationCode, abi.encode("0x00"));
+//         vm.broadcast();
     
-        MasterHook masterHook = new MasterHook{salt salt}();
-        require(address(masterHook), masterHookExpectedAddress);
+//         MasterHook masterHook = new MasterHook{salt salt}();
+//         require(address(masterHook), masterHookExpectedAddress);
     
-        (address allHookImplExpectedAddress, bytes salt2) = HookMiner.find(
-            CREATE2_DEPLOYER, Hooks.ALL_HOOK_MASK, type(AllHook).creationCode, constructorArgs
-        );
+//         (address allHookImplExpectedAddress, bytes salt2) = HookMiner.find(
+//             CREATE2_DEPLOYER, Hooks.ALL_HOOK_MASK, type(AllHook).creationCode, constructorArgs
+//         );
 
-        vm.broadcast();
-        AllHook allHook = new AllHook{salt:salt2}(IPoolManager(POOLMANAGER));
-        require(address(allHook),allHookImplExpectedAddress);
+//         vm.broadcast();
+//         AllHook allHook = new AllHook{salt:salt2}(IPoolManager(POOLMANAGER));
+//         require(address(allHook),allHookImplExpectedAddress);
 
-    }
+//     }
 
-}
+// }
--- contracts/hook-master-pkg/test/MasterHook.t.sol
+++ contracts/hook-master-pkg/test/MasterHook.t.sol
@@ -1,79 +1,90 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// import {Test, console2} from "forge-std/Test.sol";
-// import "@uniswap/v4-periphery/test/shared/PosmTestSetup.sol";
-// import {IMasterHook} from "../src/interfaces/IMasterHook.sol";
-// import  "@uniswap/v4-periphery/src/utils/HookMiner.sol";
-// import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
-// import {AllHook} from "../src/AllHook.sol";
-// import {Constants} from "@uniswap/v4-core/test/utils/Constants.sol";
-// import {TickMath} from "@uniswap/v4-core/src/libraries/TickMath.sol";
+// import {MasterHook} from "../src/MasterHook.sol";
+import {MasterHook0_8_26} from "./helpers/MasterHook0_8_26.sol";
+import {Test, console2} from "forge-std/Test.sol";
+import "@uniswap/v4-periphery/test/shared/PosmTestSetup.sol";
+import {IMasterHook} from "../src/interfaces/IMasterHook.sol";
+import  "@uniswap/v4-periphery/src/utils/HookMiner.sol";
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
+import {AllHook} from "../src/AllHook.sol";
+import {Constants} from "@uniswap/v4-core/test/utils/Constants.sol";
+import {TickMath} from "@uniswap/v4-core/src/libraries/TickMath.sol";
 // import {PoolSwapTest} from "@uniswap/v4-core/src/test/PoolSwapTest.sol";
 // import {ModifyLiquidityParams, SwapParams} from "@uniswap/v4-core/src/types/PoolOperation.sol"; 
 // import  "../script/DeployMasterHook.s.sol";
 
-// contract MasterHookTest is Test,PosmTestSetup{
-//     address master_hook;
+contract MasterHookTest is Test, PosmTestSetup{
+    MasterHook0_8_26 master_hook;
     
-//     address protocol_admin = makeAddr("protocolAdmin");
-//     address any_caller = makeAddr("anyCaller");
-//     AllHook all_hook;
-//     PoolKey poolKey;
-//     uint160 initSqrtPriceX96;
+    address protocol_admin = makeAddr("protocolAdmin");
+    address any_caller = makeAddr("anyCaller");
+    AllHook all_hook;
+    PoolKey poolKey;
+    uint160 initSqrtPriceX96;
 
-//     function setUp() public{
-//         deployFreshManagerAndRouters();
-//         deployMintAndApprove2Currencies();
-//         deployAndApprovePosm(manager); 
-//         all_hook = AllHook(
-//             address(uint160(type(uint160).max & clearAllHookPermissionsMask | Hooks.ALL_HOOK_MASK))
-//         );
-//         deployCodeTo("AllHook", abi.encode(manager), address(all_hook));
+    function setUp() public{
+        deployFreshManagerAndRouters();
+        deployMintAndApprove2Currencies();
+        deployAndApprovePosm(manager); 
+
+        all_hook = AllHook(
+            address(uint160(type(uint160).max & clearAllHookPermissionsMask | Hooks.ALL_HOOK_MASK))
+        );
+        deployCodeTo("AllHook", abi.encode(manager), address(all_hook));
+        
+        vm.startPrank(protocol_admin);
+
+        master_hook = MasterHook0_8_26(
+            address(uint160(type(uint160).max & clearAllHookPermissionsMask | Hooks.ALL_HOOK_MASK))
+        );
+
+        deployCodeTo("MasterHook0_8_26", abi.encode("0x00"), address(master_hook));
+        initSqrtPriceX96 = uint160(TickMath.getSqrtPriceAtTick(0));
+        poolKey = PoolKey({
+            currency0: currency0,
+            currency1: currency1,
+            fee: Constants.FEE_LOW,
+            tickSpacing: 60,
+            hooks: IHooks(address(master_hook))
+        });
+ 
+        // IMasterHook(master_hook).initialize(address(manager), address(all_hook));
+        // vm.startPrank(any_caller);
+        // master_hook.initialize(address(manager), address(all_hook));
+        // vm.stopPrank();
+
+        
+        }
+
+
+        vm.stopPrank();
+
+        deal(Currency.unwrap(currency0), any_caller, uint256(type(uint112).max));
+        deal(Currency.unwrap(currency1), any_caller, uint256(type(uint112).max));
+        vm.deal(any_caller, 1 ether);
+        vm.deal(protocol_admin, 1 ether);
+
+//     }
+
+    function test__unit__initializeMustSucceed() public {
+//         //==========PRE-CONDITIONS==================
+//         vm.startPrank(protocol_admin);
+//         vm.expectRevert(BaseHook.HookNotImplemented.selector);
+//         manager.initialize(poolKey, initSqrtPriceX96);
+//         vm.stopPrank();
+
+
+// //         //==============TEST========================
         
 //         vm.startPrank(protocol_admin);
-
-//         (address _expectedMasterHook, bytes32 _salt) = HookMiner.find(protocol_admin, Hooks.ALL_HOOK_MASK, MASTER_HOOK_BYTECODE, abi.encode(manager));
         
-//         assembly{
-//             master_hook := create2(0x00, add(MASTER_HOOK_BYTECODE, 0x20), mload(MASTER_HOOK_BYTECODE), _salt)
-//         }
-
-
+//         IMasterHook(master_hook).initialize(address(manager), address(all_hook));
 //         vm.stopPrank();
-//         initSqrtPriceX96 = uint160(TickMath.getSqrtPriceAtTick(0));
-//         initPoolAndAddLiquidity(currency0, currency1, IHooks(master_hook), Constants.FEE_LOW, initSqrtPriceX96);
-
-//         deal(Currency.unwrap(currency0), any_caller, uint256(type(uint112).max));
-//         deal(Currency.unwrap(currency1), any_caller, uint256(type(uint112).max));
-//         vm.deal(any_caller, 1 ether);
-//         vm.deal(protocol_admin, 1 ether);
-
-//     }
-
-//     function test__unit__initializeMustSucceed() public {
-//         //==========PRE-CONDITIONS==================
-//         // PoolSwapTest.TestSettings memory testSettings =
-//         //     PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false});
-//         // vm.startPrank(any_caller);
-//         // vm.expectRevert();
-
-//         // swapRouter.swap(
-//         //     poolKey,
-//         //     SwapParams(true,-int256(1e18),TickMath.MIN_SQRT_PRICE + 1),
-//         //     PoolSwapTest.TestSettings(false, false),
-//         //     Constants.ZERO_BYTES
-//         // );
-//         // vm.stopPrank();        
-
-//         //==============TEST========================
-        
-//         // vm.startPrank(protocol_admin);
-        
-//         // IMasterHook(master_hook).initialize(address(manager), address(all_hook));
-//         // vm.stopPrank();
 
 //         //==========POST-CONDTIONS==================
-//     }
+    }
 
-// }
+}
